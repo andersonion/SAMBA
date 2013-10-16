78b0a7a1f497cba5c079f0c5dfcffa00f0f87d1b
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2013-10-16 18:14:19 +0000

    workstation_code added an update others command, it pulls a list of hosts from the engine_deps folder in pipeline settings
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/workstation_code/trunk@1665 d41079a3-eaa1-4f09-8506-b324c47726bb

---
+++ update_others.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# simple script to run and svn update directories on each host
+
+for host in `ls pipeline_settings/engine_deps/engine_*_dependencies | cut -d '_' -f4 | sort -u`
+do
+    echo " --- updating host $host ---"
+    ssh -o ConnectTimeout=1 $host /opt/subversion/bin/svn update /Volumes/workstation_home/Software 2>&1 > svn_update_${host}.log &
+done
+
