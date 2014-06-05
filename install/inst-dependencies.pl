44658a02ad0dddb69b5274efa1a2f13ebfb7e06c
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2014-06-05 17:44:31 +0000

    modified to use the global varaibles we expect to have set up by our main script.
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/workstation_code/trunk@2144 d41079a3-eaa1-4f09-8506-b324c47726bb

--- install/inst-dependencies.pl
+++ install/inst-dependencies.pl
@@ -1,6 +1,6 @@
 sub dependencies (){
     print("dependencies\n");
-    return 1;
+
 ###
 # update engine_something_dependencies.
 ###
@@ -9,12 +9,13 @@ sub dependencies (){
 # cp engine_generic_dependincies to engine_hostname_depenendinceis and 
 # link it to pipeline_dependencies and recon_dependencies.
 ###
+    
 	print("---\n");
 	print("Setting engine dependencies ...... \n");
 	print("---\n");
 	print("setting engine dependencies\n");
-	my $dep_file="${wks_home}/pipeline_settings/engine_deps/engine_${hostname}_dependencies";
-	my $default_file="${wks_home}/pipeline_settings/engine_deps/engine_generic_dependencies";
+	my $dep_file="${WKS_HOME}/pipeline_settings/engine_deps/engine_${HOSTNAME}_dependencies";
+	my $default_file="${WKS_HOME}/pipeline_settings/engine_deps/engine_generic_dependencies";
 	if( -e $dep_file && ! -e "${dep_file}.bak") { 
 	    `mv $dep_file ${dep_file}.bak`; 
 	    `cp $default_file $dep_file`;
@@ -29,14 +30,14 @@ sub dependencies (){
 	}
 # sub hostname for hostname_radish  and hostname_pipeline
 	my( $rdep, $pdep);
-	($rdep = $dep_file ) =~ s/$hostname/${hostname}_radish/gx;
-	($pdep = $dep_file )=~ s/$hostname/${hostname}_pipeline/gx;
+	($rdep = $dep_file ) =~ s/$HOSTNAME/${HOSTNAME}_radish/gx;
+	($pdep = $dep_file )=~ s/$HOSTNAME/${HOSTNAME}_pipeline/gx;
 	for my $file ($rdep, $pdep) {
 #	    if( ! -e  $file) {
 	    chdir dirname($dep_file);
 	    my $ln_cmd="ln -fs ".basename($dep_file)." ".basename($file);
 	    `$ln_cmd`;
-	    chdir $wks_home;
+	    chdir $WKS_HOME;
 		#`ln -s $dep_file $pdep`;
 		print ("made link for legacy code\n\t$file");
 #	    } else { 
@@ -63,40 +64,40 @@ sub dependencies (){
 # # wrkstation workflow settings file
 # # format like headfile
 	    if ( $line =~ /^engine=hostname/x ) { 
-		$string="engine=$hostname";
+		$string="engine=$HOSTNAME";
 # engine=hostname
 # engine_endian=little
 # ###
 # # data_locations
 # engine_work_directory=/Volumes/enginespace
 	    } elsif ($line =~ /^engine_work_directory=/x ) {
-		$string="engine_work_directory=/${hostname}space";
+		$string="engine_work_directory=/${HOSTNAME}space";
 # engine_data_directory=/Volumes/worktation_data/data
 	    } elsif ($line =~ /^engine_data_directory=/x ) {
 		$string="engine_data_directory=$data_home";
-# engine_recongui_paramfile_directory=/wks_home/dir_param_files
+# engine_recongui_paramfile_directory=/WKS_HOME/dir_param_files
 	    } elsif ($line =~ /^engine_recongui_paramfile_directory=/x ) {
-		$string="engine_recongui_paramfile_directory=$wks_home/dir_param_files";
+		$string="engine_recongui_paramfile_directory=$WKS_HOME/dir_param_files";
 		
-# engine_recongui_menu_path=/wks_home/pipe_settings/recon_menu.txt
+# engine_recongui_menu_path=/WKS_HOME/pipe_settings/recon_menu.txt
 	    } elsif ($line =~ /^engine_recongui_menu_path=/x ) {
-		$string="engine_recongui_menu_path=$wks_home/pipeline_settings/recon_menu.txt";
+		$string="engine_recongui_menu_path=$WKS_HOME/pipeline_settings/recon_menu.txt";
 		
 # engine_archive_tag_directory=/engine_work_directory/Archive_Tags
 	    } elsif ($line =~ /^engine_archive_tag_directory=/x ) {
-		$string="engine_archive_tag_directory=/Volumes/${hostname}space/Archive_Tags";
-		if ( ! -d "/Volumes/${hostname}space/Archive_Tags") {
-		    `mkdir "/Volumes/${hostname}space/Archive_Tags"`;
+		$string="engine_archive_tag_directory=/Volumes/${HOSTNAME}space/Archive_Tags";
+		if ( ! -d "/Volumes/${HOSTNAME}space/Archive_Tags") {
+		    `mkdir "/Volumes/${HOSTNAME}space/Archive_Tags"`;
 		    }
-# engine_waxholm_canonical_images_dir=/wks_home/whs_references/whs_canonical_images/alx_can_101103
+# engine_waxholm_canonical_images_dir=/WKS_HOME/whs_references/whs_canonical_images/alx_can_101103
 	    } elsif ($line =~ /^engine_waxholm_canonical_images_dir=/x ) {
 		$string="engine_waxholm_canonical_images_dir=$data_home/atlas/whs2";
-# engine_waxholm_labels_dir=/wks_home/whs_references/whs_labels/canon_labels_101103
+# engine_waxholm_labels_dir=/WKS_HOME/whs_references/whs_labels/canon_labels_101103
 	    } elsif ($line =~ /^engine_waxholm_labels_dir=/x ) {
 		$string="engine_waxholm_labels_dir=$data_home/atlas/whs2";
-# engine_app_dti_recon_param_dir=/wks_home/dti_references
+# engine_app_dti_recon_param_dir=/WKS_HOME/dti_references
 	    } elsif ($line =~ /^engine_app_dti_recon_param_dir=/x ) {
-		$string="engine_app_dti_recon_param_dir=$wks_home/pipeline_settings/tensor";
+		$string="engine_app_dti_recon_param_dir=$WKS_HOME/pipeline_settings/tensor";
 # #
 		
 # ###
@@ -108,12 +109,12 @@ sub dependencies (){
 		
 # ###
 # # program locations
-# engine_radish_bin_directory=/wks_home/recon/legacy/modules/bin_macINTEL
+# engine_radish_bin_directory=/WKS_HOME/recon/legacy/modules/bin_macINTEL
 	    } elsif ($line =~ /^engine_radish_bin_directory=/x ) {
-		$string="engine_radish_bin_directory=$wks_home/bin";#recon/legacy/modules/_mac_${arch}
-# engine_radish_contributed_bin_directory=/wks_home/recon/legacy/modules/contributed/bin_macINTEL 
+		$string="engine_radish_bin_directory=$WKS_HOME/bin";#recon/legacy/modules/_mac_${arch}
+# engine_radish_contributed_bin_directory=/WKS_HOME/recon/legacy/modules/contributed/bin_macINTEL 
 	    } elsif ($line =~ /^engine_radish_contributed_bin_directory=/x ) {
-		$string="engine_radish_contributed_bin_directory=$wks_home/recon/legacy/modules/contributed/bin_mac_${arch}";
+		$string="engine_radish_contributed_bin_directory=$WKS_HOME/recon/legacy/modules/contributed/bin_mac_${arch}";
 # engine_app_matlab=/usr/bin/matlab
  	    } elsif ($line =~ /^engine_app_matlab=/x ) { 
  		$string="engine_app_matlab=/usr/bin/matlab";# -nosplash -nodisplay -nodesktop ";
@@ -122,10 +123,10 @@ sub dependencies (){
  		$string="engine_app_matlab_opts=-nosplash -nodisplay -nodesktop";
 # engine_app_ants_dir=/Applications/SegmentationSoftware/ANTS/
 	    } elsif ($line =~ /^engine_app_ants_dir=/x ) { 
-		$string="engine_app_ants_dir=/$wks_home/../usr/bin/";
+		$string="engine_app_ants_dir=/$WKS_HOME/../usr/bin/";
 # engine_app_fsl_dir=/Applications/SegmentationSoftware/fsl/bin
 	    } elsif ($line =~ /^engine_app_fsl_dir=/x ) {
-		$string="engine_app_fsl_dir=$wks_home/../fsl/bin";
+		$string="engine_app_fsl_dir=$WKS_HOME/../fsl/bin";
 # engine_app_dti_recon=/Applications/Diffusion\ Toolkit.app/Contents/MacOS/dti_recon
 # engine_app_dti_tracker=/Applications/Diffusion\ Toolkit.app/Contents/MacOS/dti_tracker
 # engine_app_dti_spline_filter=/Applications/Diffusion\ Toolkit.app/Contents/MacOS/spline_filter
@@ -152,6 +153,6 @@ sub dependencies (){
 	print("moving $outpath to $inpath\n");
 	`mv $outpath $inpath`;
 #`mv  ${dep_file}.bak $dep_file`
-	return;
+	return 1;
 }
 1;
