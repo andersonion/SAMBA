93dbdf5cd5fef349c830c400fb7c3749ab681fd2
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2014-06-05 19:04:37 +0000

    enabled usage, will prompt non mac users for imagej location
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/workstation_code/trunk@2151 d41079a3-eaa1-4f09-8506-b324c47726bb

--- install/inst-ij_macros.pl
+++ install/inst-ij_macros.pl
@@ -1,20 +1,42 @@
+use warnings;
+#use strict;
 sub ij_macros() {
     print("ij_macros\n");
-    return 1;
+
 ### 
-# some more linking
+# set up imagej macro link
 ###
-    $infile="$wks_home/analysis/james_imagejmacros";
+    my $infile;
+    my $ln_source;
+    my $ln_dest;
+    my $ln_cmd;
+    $infile="$WKS_HOME/analysis/";
+    $ln_source="$infile"."james_imagejmacros";;
     
-    $ln_source="$infile";
-    $ln_dest="/Applications/ImageJ/plugins/000_james_imagejmacros";
-    if ( -r $ln_dest ) { 
-	`unlink $ln_dest`;
-    }    
-    $ln_cmd="ln -sf $ln_source $ln_dest";
-    #print ("$ln_cmd\n");
-    `$ln_cmd`;
-    return;
+    if($IS_MAC  ){
+	$ln_dest="/Applications/ImageJ/plugins/000_james_imagejmacros";
+    } else {
+	print("ImageJMacro link location?\n\tThe full path to imagej/plugins/ijmacros\n");
+	$ln_dest=readline(*STDIN);
+	chomp($ln_dest);
+    }
+
+    if ( ! length ($ln_dest)) {
+	undef $ln_dest;
+    } elsif ( -e $ln_dest ) { 
+	unlink($ln_dest);
+    } 
+    if (! -d $ln_source  ) {
+	undef $ln_source;
+    }
+    if ( defined($ln_source) && defined($ln_dest) ){
+	$ln_cmd="ln -sf $ln_source $ln_dest";
+	print ("$ln_cmd\n");
+	#`$ln_cmd`;
+	return 1;
+    } else {
+	return 0;
+    }
 }
 1;
 
