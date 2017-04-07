0c91d2388e0e4cb64da6c3dadee034a38eaca930
BJ Anderson <rja20@civmcluster1.cm.cluster>
2017-04-07 09:21:22 -0400

    First semi-functional end-to-end usable version of single_segmentation.
    This includes pulling tensor_create results data from a specific recon machine, and calculating a combine bval/bvec b-table from the headefile in the case that tensor_create work data is no longer there (this was the case with my test data, otherwise I probably would not have gone this route).  This means that everything that is needed can be pulled over in one shot by transferring the whole tensor-results folder.  On the other end, the bvecs are transformed, and if we decide to convert to RAS, are copied into the results directory.  This is still imperfect, and doesn't do a good job of file checking and then acting appropriately.
    
    Code still needs to be cleaned up, and better handling of converting to RAS needs to be implemented as well.  Also should be noted that there needs to be a concurrent update with civm_matlab_utilities folder (or whatever it is exactly called).  Unsure if any there are any dependencies on recent changes to pipeline_utilities.
    
    
    Former-commit-id: 6833e0c117aa7fb87c2ba12a1abd361e027ecf1c

--- single_segmentation_start.pl
+++ single_segmentation_start.pl
@@ -255,13 +255,14 @@ $image_dimensions
 
 #$reference_path -- DEFUNCT?
     $create_labels=1;
-    #$label_space='post_rigid';
-    $label_space='pre_rigid';
+    $label_space='post_rigid';
+    #$label_space='pre_rigid';
     #$label_reference
     $convert_labels_to_RAS = 1;
     $vbm_reference_space='native';
     $do_connectivity = 1;
 
+    $eddy_current_correction = 1;
 
 
 ## Add tensor preprocessing here...pulling in all data including nii4D and bvecs and ECC affine matrices
@@ -279,29 +280,222 @@ sub pull_data_for_connectivity {
 #---------------------
     my $complete_runno_list=$Hf->get_value('complete_comma_list');
     my @array_of_runnos = split(',',$complete_runno_list);
+
+    my $complete_channel_list=$Hf->get_value('channel_comma_list');
+    my @array_of_channels = split(',',$complete_channel_list);
+
     my $inputs_dir = $Hf->get_value('pristine_input_dir');
-    print "inputs_dir = ${inputs_dir}\n\n";
+    my $message_prefix="single_segmentation_start::pull_data_for_connectivity:\n";
+    my $message_body;
+
     foreach my $runno(@array_of_runnos) {
-       
+	my $local_message_prefix="Attempting to retrieve data for runno: ${runno}\n";
+	my $log_msg;
+	my $tmp_log_msg;
+	my $look_in_local_folder = 0;
+	my $local_folder = "${inputs_dir}/tmp_folder/";
+
+	# Look for more then two xform_$runno...mat files (ecc affine transforms)
+	if ((defined $eddy_current_correction) && ($eddy_current_correction == 1)) {
+	   my $number_of_ecc_xforms =  `ls ${inputs_dir}/xform_${runno}*.mat | wc -l`;
+	   
+	   print "number_of_ecc_xforms = ${number_of_ecc_xforms}\n\n";
+	   if ($number_of_ecc_xforms < 6) { # For DTI, the minimum number of non-b0's is 6!
+	       $tmp_log_msg = `puller_simple  -or ${recon_machine} tensor${runno}*-DTI-results/ ${local_folder}/`;
+	       $log_msg = $log_msg.$tmp_log_msg;
+	       $tmp_log_msg = `mv ${local_folder}/xform*mat ${inputs_dir}`;
+	       $log_msg = $log_msg.$tmp_log_msg;
+	       $look_in_local_folder = 1;
+	   }
+	   
+	}
+
+
+	# get any specified "traditional" dti images
+	foreach my $contrast (@array_of_channels) {
+	    my $test_file =  get_nii_from_inputs($inputs_dir,$runno,$contrast);
+	    my $pull_file_cmd = "puller_simple -f file -or ${recon_machine} tensor${runno}*-DTI-results/${runno}*${contrast}.nii* ${inputs_dir}/";
+	    
+	    if ($test_file =~ /[\n]+/) {
+		if ($look_in_local_folder) {
+		    $test_file =  get_nii_from_inputs($local_folder,$runno,$contrast);
+		    if ($test_file =~ /[\n]+/) {
+			$tmp_log_msg = `${pull_file_cmd}`;
+			$log_msg = $log_msg.$tmp_log_msg;
+		    } else {
+			$tmp_log_msg = `mv ${test_file} ${inputs_dir}`;
+			$log_msg = $log_msg.$tmp_log_msg;
+		    }
+		} else {
+		    $tmp_log_msg = `${pull_file_cmd}`;
+		    $log_msg = $log_msg.$tmp_log_msg;
+		}
+	    }
+	}
+	
+	# get nii4D
 	my $nii4D = get_nii_from_inputs($inputs_dir,$runno,'nii4D');
 	my $orig_nii4D;
 	if ($nii4D =~ /[\n]+/) {
 	    $orig_nii4D =  get_nii_from_inputs($inputs_dir,'nii4D',$runno); # tensor_create outputs nii4D_$runno.nii.gz
 	    if ($orig_nii4D =~ /[\n]+/) {
-	   # print "Unable to find input image for $runno and $ch in folder: ${inputs_dir}.\n";
-	    my $cmd = `puller_simple -or ${recon_machine} tensor${runno}*-DTI-results/ ${inputs_dir}/`;
-	   
-	    `mv ${inputs_dir}/tensor${runno}*-DTI-results/*  ${inputs_dir}/`;
-	    `rm -r  ${inputs_dir}/tensor${runno}*-DTI-results/`;
-	    $orig_nii4D =  get_nii_from_inputs($inputs_dir,'nii4D',$runno); # tensor_create outputs nii4D_$runno.nii.gz
+		my $pull_nii4D_cmd = `puller_simple -f file -or ${recon_machine} tensor${runno}*-DTI-results/nii4D_${runno}*.nii* ${inputs_dir}/`;
+		if ($look_in_local_folder) {
+		    my $test_file =  get_nii_from_inputs($local_folder,'nii4D',$runno);
+		    if ($test_file =~ /[\n]+/) {
+			$tmp_log_msg = `${pull_nii4D_cmd}`;
+			$log_msg = $log_msg.$tmp_log_msg;
+		    } else {
+			$tmp_log_msg = `mv ${test_file} ${inputs_dir}`;
+			$log_msg = $log_msg.$tmp_log_msg;
+		    }
+		} else {
+		    $tmp_log_msg = `${pull_nii4D_cmd}`;
+		    $log_msg = $log_msg.$tmp_log_msg;
+		}
 	    }
+	    $orig_nii4D =  get_nii_from_inputs($inputs_dir,'nii4D',$runno); # tensor_create outputs nii4D_$runno.nii.gz
+
 	    my $new_nii4D = "${inputs_dir}/${runno}_nii4D.nii";
 	    if ($orig_nii4D =~ /'.gz'/) {
 		$new_nii4D = $new_nii4D.'.gz';
 	    }
-	    `mv ${orig_nii4D} ${new_nii4D}`;
+	    $tmp_log_msg = `mv ${orig_nii4D} ${new_nii4D}`;
+	    $log_msg = $log_msg.$tmp_log_msg;
+	}
+	
+	# get headfile
+	my $head_file = "${inputs_dir}/tensor${runno}*.headfile";
+	my $number_of_headfiles =  `ls ${head_file} | wc -l`;
+	if ($number_of_headfiles < 1) {
+	    my $pull_headfile_cmd = "puller_simple -f file -or ${recon_machine} tensor${runno}*-DTI-results/tensor${runno}*headfile ${inputs_dir}/";
+	    if ($look_in_local_folder) {
+		$head_file = "${local_folder}/tensor${runno}*.headfile";
+		$number_of_headfiles =  `ls ${head_file} | wc -l`;
+		if ($number_of_headfiles < 1) {
+		    $tmp_log_msg = `${pull_headfile_cmd}`;
+		    $log_msg = $log_msg.$tmp_log_msg;
+		} else {
+		    $tmp_log_msg = `mv ${head_file} ${inputs_dir}`;
+		    $log_msg = $log_msg.$tmp_log_msg;
+		    }
+	    } else {
+		$tmp_log_msg = `${pull_headfile_cmd}`;
+		$log_msg = $log_msg.$tmp_log_msg;
+	   }
+	    
+	}
+
+
+	# Clean up temporary results folder
+	if ($look_in_local_folder) {
+	    #my $temp_folder = "${inputs_dir}/tensor${runno}*-DTI-results/";
+	    my $number_of_temp_results_folders =  `ls -d ${head_file} | wc -l`;
+	    if ($number_of_headfiles > 0) {
+		$tmp_log_msg = `rm -r ${local_folder}`;
+		$log_msg = $log_msg.$tmp_log_msg;
+	    }
+	}
+
+	# Figure out which bvecs/bvals file to get
+	my @headfiles = `ls ${inputs_dir}/tensor${runno}*headfile`;
+
+	my $current_headfile = $headfiles[0];
+	#my $current_Hf = read_headfile($current_headfile);
+	my $current_Hf = new Headfile ('rw', $current_headfile);
+	$current_Hf->read_headfile;
+	my $original_gradient_location = $current_Hf->get_value('dti-recon-gradmat-file'); ## Unsure if this will work for Bruker...
+	my ($o_grad_path,$grad_filename,$grad_ext) = fileparts($original_gradient_location,2);
+	my $gradient_file = "${inputs_dir}/${runno}_${grad_filename}${grad_ext}";
+	if (data_double_check($gradient_file)) { # Try pulling from tensor work folder first
+	    my $bvec_machine;
+	    if ($o_grad_path =~ s/^(\/){1}([A-Za-z1-9]*)(space\/)//){
+		$bvec_machine = $2;
+	    } else {
+		$bvec_machine = $recon_machine;
+	    }
+	    my $pull_bvecs_cmd = "puller_simple -f file -or ${bvec_machine} ${o_grad_path}/${grad_filename}${grad_ext} ${gradient_file}";
+	    $tmp_log_msg = `${pull_bvecs_cmd}`;
+	    $log_msg = $log_msg.$tmp_log_msg;
+	}
+	
+	if (data_double_check($gradient_file)) { #If unable to pull in from tensor work folder, create bvecs from info in headfile 
+	    # This code is based on the shenanigans of tensor_create, as found in main_tensor.pl
+	    my $Hf_grad_info =  $current_Hf->get_value("gradient_matrix_auto");
+	    #parse bvecs
+	    my ($grad_dim_info,$Hf_grad_string) = split(',',$Hf_grad_info);
+	    my @Hf_gradients = split(' ',$Hf_grad_string);
+	    my ($num_bvecs,$v_dim) = split(':',$grad_dim_info);
+	    
+	    #parse bvals
+	    my $Bruker_data = 0; ### Temporarily only supporting Agilent data!
+	    
+	    my $approx_Hf_bval_handle;
+	    my $Hf_bval_handle;
+	    if (! $Bruker_data) { # Right now (06 April 2017) we're assuming Agilent data
+		$approx_Hf_bval_handle = "z_Agilent_bvalue";
+	    }
+	    my $Hf_bval_info = $current_Hf->get_value_like($approx_Hf_bval_handle);
+	    my ($bval_dim_info,$Hf_bval_string) = split(',',$Hf_bval_info);
+	    my @Hf_bvals = split(' ',$Hf_bval_string);
+	    my ($num_bvals,$bval_dim) = split(':',$bval_dim_info);
+	    my $single_bval =0;
+	    if ($num_bvals != $num_bvecs) { # If stuff blows up, let's default to assuming a single max_bvalue
+		$single_bval=1;
+		$approx_Hf_bval_handle = "max_bval";
+		$Hf_bval_info = $current_Hf->get_value_like($approx_Hf_bval_handle);
+		$Hf->set_value("max_bvalue_${runno}",$Hf_bval_info);
+	    }
+
+	    # combine bvals and bvecs into one table
+
+	    my @gradient_matrix;
+	    for (my $bb=0;($bb < $num_bvecs); $bb++) {
+		$tmp_log_msg = "Creating combined bval/bvec b-table from headfile: ${current_headfile}.";
+		$log_msg = $tmp_log_msg;
+		
+		my @temp_array;
+		my $nonzero_test = 0;
+		for (my $ii=0; ($ii < $v_dim); $ii++) {
+		    my $temp_val = shift(@Hf_gradients);
+		    push(@temp_array,$temp_val);
+		    if (! $nonzero_test) {
+			if ($temp_val ne '0') { # We are assuming that zero will always be stored in headfile as '0' (nor '0.0', '0.000', etc.
+			    $nonzero_test = 1;
+			}
+		    }
+		}
+		my $current_bval=0;
+		if ($single_bval) {
+		    if ($nonzero_test) {
+			$current_bval = $Hf_bval_info;
+		    }
+		} else {
+		    my $new_bval = shift(@Hf_bvals);
+		    if ($nonzero_test) {
+			$current_bval = $new_bval;
+		    }
+		}
+
+		my $b_string = join(', ',($current_bval,@temp_array));
+		push(@gradient_matrix,$b_string."\n");
+		$tmp_log_msg = ".";
+		$log_msg = $tmp_log_msg;
+	    }
+	    $tmp_log_msg = "\nDone creating b-table: ${gradient_file} for ${num_bvecs} bval/bvec entries.\n";
+	    $log_msg = $tmp_log_msg;
+   	    	    
+	    write_array_to_file($gradient_file,\@gradient_matrix);
+	}
+	$Hf->set_value("original_bvecs_${runno}",$gradient_file);
+	# log any messages
+	
+	if ($log_msg ne '') {
+	    $message_body=$message_body."\n".$local_message_prefix.$log_msg;
 	}
     }
-    die;#
+    if ($message_body ne '') {
+	log_info("${message_prefix}${message_body}");
+    }
 }
    
