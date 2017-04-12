2e6a3700b2d16fc33e3b5bac9c0960fbccebb73a
BJ Anderson <rja20@civmcluster1.cm.cluster>
2017-04-12 15:16:30 -0400

    Had put the previous memory request in the wrong subfunction...fixed that now.

--- mask_images_vbm.pm
+++ mask_images_vbm.pm
@@ -252,11 +252,9 @@ sub port_atlas_mask_vbm {
     
     my @test = (0);
     my $node = '';
-    my $mem_request = 60000; # 12 April 2017, BJA: added higher memory request because of nii4Ds...may need to even go higher, but really should do this smartly.
     if (defined $reservation) {
 	@test =(0,$reservation);
     }
-
 
 
     my $jid = 0;
@@ -264,7 +262,7 @@ sub port_atlas_mask_vbm {
 	my ($home_path,$dummy1,$dummy2) = fileparts($port_mask,2);
 	my $Id= "${runno}_create_port_atlas_mask";
 	my $verbose = 2; # Will print log only for work done.
-	$jid = cluster_exec($go, $go_message, $cmd,$home_path,$Id,$verbose,$mem_request,@test);     
+	$jid = cluster_exec($go, $go_message, $cmd,$home_path,$Id,$verbose,'',@test);     
 	if (! $jid) {
 	    error_out($stop_message);
 	}
@@ -307,6 +305,8 @@ sub mask_one_image {
     if (defined $reservation) {
 	@test =(0,$reservation);
     }
+
+    my $mem_request = 60000; # 12 April 2017, BJA: added higher memory request because of nii4Ds...may need to even go higher, but really should do this smartly.
     
     my $jid = 0;
     if (cluster_check) {
@@ -317,7 +317,7 @@ sub mask_one_image {
 	my $home_path = $current_path;
 	my $Id= "${runno}_${ch}_apply_${template_contrast}_mask";
 	my $verbose = 2; # Will print log only for work done.
-	$jid = cluster_exec($go,$go_message, $cmd ,$home_path,$Id,$verbose,'',@test);     
+	$jid = cluster_exec($go,$go_message, $cmd ,$home_path,$Id,$verbose,$mem_request,@test);     
 	if (! $jid) {
 	    error_out($stop_message);
 	}
