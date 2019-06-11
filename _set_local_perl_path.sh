cc3902887d25fdfeb3e4b5e1798bfd9e7e27799d
rja20 <rja20@blade02.dhe.duke.edu>
2019-06-11 13:34:49 -0400

    Pre-merge commit.

--- _set_local_perl_path.sh
+++ _set_local_perl_path.sh
@@ -5,10 +5,10 @@ new_perl_path=${new_perl_path//\//\\\/}; # Protect the slashes
 
 for perl_script in $(ls ${SAMBA_dir}/*.pl);do
 echo "Attempting to fix perl path in: ${perl_script}";
-   perl -pi -e "s/\#\!(.*)$/${new_perl_path}/g"  ${perl_script};
+   perl -pi -e "s/^\#\!.*$/\#\!${new_perl_path}/g"  ${perl_script};
 done
 
 for perl_mod in $(ls ${SAMBA_dir}/*.pm);do
 echo "Attempting to fix perl path in: ${perl_mod}";
-   perl -pi -e "s/\#\!(.*)$/${new_perl_path}/g"  ${perl_mod};
+   perl -pi -e "s/^\#\!.*$/\#\!${new_perl_path}/g"  ${perl_mod};
 done
