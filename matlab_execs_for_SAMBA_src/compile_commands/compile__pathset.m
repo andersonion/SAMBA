d50379598f84fc5240c81ed755b19966106b3e38
James Cook <jamesjcook@users.noreply.github.com>
2019-09-05 17:35:28 -0400

    Added missing compile__pathset

---
+++ matlab_execs_for_SAMBA_src/compile_commands/compile__pathset.m
@@ -0,0 +1,5 @@
+[pdir]=fileparts(fileparts(mfilename('fullpath')));
+addpath(fullfile(pdir,''));
+addpath(fullfile(pdir,'..'));
+addpath(fullfile(pdir,'..','fdr_bh'));
+addpath(fullfile(pdir,'..','20130227_xlwrite'));
