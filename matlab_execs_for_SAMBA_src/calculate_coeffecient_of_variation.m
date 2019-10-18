0580292f06fd41424aaef71848d69cdb96933e5f
James Cook <jamesjcook@users.noreply.github.com>
2019-10-18 15:27:25 -0400

    Overhaul of syntax and internal structure for clarity
    Output should be identical to previous version

--- matlab_execs_for_SAMBA_src/calculate_coeffecient_of_variation.m
+++ matlab_execs_for_SAMBA_src/calculate_coeffecient_of_variation.m
@@ -1,18 +1,14 @@
-function [ CoV_array ] = calculate_coeffecient_of_variation( individual_stat_file,field, delta )
-% CoV_triplets = CALCULATE_COEFFECIENT_OF_VARIATION Smartly calculate CoV between L/R ROIs
-%   Input an individual stat file as produced by VBA, etc pipeline
-%   Define the field (vol, fa, etc) on which to work
-%   Use a difference constant 'delta' to map L-R pairs
-% Output is 3xN array of roi,cov,mean for each roi found
+function CoV_array = calculate_coeffecient_of_variation( individual_stat_file,field, delta )
+% CoV_triplets = CALCULATE_COEFFECIENT_OF_VARIATION( stat_file, field, delta )
+% Calculate CoV between L/R ROIs  (std/mean)
+%   Input:
+%   stat file - as produced by write_individual_stats,
+%     ( tab separated, loadable by matlab's readtable with a header line) 
+%   field -     which column we're comparing
+%   delta -     the difference constant to map L-R pairs
+% CoV_triplets output are 3xN array of roi,cov,mean for each roi found
 
-%individual_stat_file='/cm/shared/CIVMdata/atlas/xmas2015rat_symmetric_cropped/xmas2015rat_symmetric_cropped_xmas2015rat_symmetric_cropped_labels_in_native_space_stats.txt';
-%individual_stat_file='/civmnas4/rja20/data_from_glusterspace/VBM_17gaj40_chass_symmetric2_CAST-work/dwi/SyN_0p25_3_0p5_fa/faMDT_all_n8_i6/stats_by_region/labels/pre_rigid_native_space/CAST_symmetric_R/stats/individual_label_statistics/N54823_CAST_symmetric_R_labels_in_native_space_stats.txt';
 isf=individual_stat_file;
-%field='volume_mm3';
-%delta=1;
-
-do_process=1;
-CoV_array=[];
 if exist(isf, 'file')
     % this line fails when we have header lines... like in the old days : (
     master_T = readtable( isf,'ReadVariableNames',1,'HeaderLines',0,'Delimiter','\t');
@@ -29,80 +25,88 @@ if exist(isf, 'file')
     clear master_T_o;
 else
     error('Missing file %s',isf);
-    do_process=0;
 end
-if do_process
-    existing_fields = master_T.Properties.VariableNames;
-    explicit_ROIs=1;
-    if ismember('ROI',existing_fields)
-        raw_array=master_T.ROI;
+existing_fields = master_T.Properties.VariableNames;
+if exist('field','var')
+    if ismember(field,existing_fields)
+        c_field=field;
+    elseif ismember([field '_mean'],existing_fields)
+        c_field=[field '_mean'];
+    elseif str2num(field) <= numel(existing_fields)
+        c_field=field{str2num(field)};
     else
-        explicit_ROIs=0;
+        disp(['Error: field "' field '" not found in input file. Dying now.'])
+        return;
     end
-    if exist('field','var')
-        if ismember(field,existing_fields)
-            c_field=field;
-        elseif ismember([field '_mean'],existing_fields)
-            c_field=[field '_mean'];
-        elseif str2num(field) <= numel(existing_fields)
-            c_field=field{str2num(field)};
-        else
-            disp(['Error: field "' field '" not found in input file. Dying now.'])
-            return;
-        end
-    else
-        c_field=existing_fields{2};
-    end
-    
-    raw_array=[raw_array,master_T.(c_field)]';
-    
-    
-    n_ROIs=size(raw_array,2);
-    odd_or_even=mod(n_ROIs,2);
-    
-    % If odd, assume the first entry is whole-brain exterior '0'
-    if ((odd_or_even) || ~raw_array(1,1))
-        try
-            raw_array(:,((raw_array(1,:)==0)))=[];
-        catch
-        end
-        n_ROIs=size(raw_array,2);
-        odd_or_even=0;
-    end
-    
-    if ~explicit_ROIs
-        raw_array(1,:)=1:1:n_ROIs;
-    end
-    
-    all_ROIs = raw_array(1,:);
-    if ~exist('delta','var')
-        delta=all_ROIs(n_ROIs)-all_ROIs(round(n_ROIs/2));
-    end
-    
-    
-    field_data=raw_array(2,:);
-    processed_ROIs=[];
-    
-    intermediate_array=[];
-    for rr=1:numel(all_ROIs);
-        c_ROI=all_ROIs(1,rr);
-        if ~ismember(c_ROI,processed_ROIs)
-            c_ROI_L=c_ROI;
-            c_ROI_R=c_ROI+delta;
-            if ~ismember(c_ROI_R,processed_ROIs) &&  ismember(c_ROI_R,all_ROIs)
-                i_cRR=find(all_ROIs==c_ROI_R);
-                intermediate_array=[intermediate_array; c_ROI_L, field_data(rr),field_data(i_cRR)];
-                processed_ROIs=[processed_ROIs c_ROI_L c_ROI_R];
-            end
-        end
-    end
-    intermediate_array=intermediate_array';
-    CoV_array=zeros([2,size(intermediate_array,2)]);
-    
-    CoV_array(1,:)=intermediate_array(1,:);
-    % It is important that the second std option be set to 0 to match what Nian did in Excel.
-    CoV_array(2,:)=std(intermediate_array(2:3,:),0,1)./mean(intermediate_array(2:3,:),1); 
-    CoV_array(3,:)=mean(intermediate_array(2:3,:),1);
+else
+    c_field=existing_fields{2};
 end
-%end
+n_ROIs=size(master_T,1);
+if ~ismember('ROI',existing_fields)
+    warning('ROI not part of table! YOU HOPE ORDERING WAS PRESERVED ON LOAD!');
+    pause(3);
+    master_T.ROI=1:1:n_ROIs;
+end
+roi_data=[master_T.ROI,master_T.(c_field)];
+
+%% trim the exterior out
+singular_exterior=mod(n_ROIs,2);
+% If the the first entry is '0' (hopefully whole-brain exterior)
+if ~roi_data(1,1)
+    roi_data(roi_data(1,:)==0,:)=[];
+    if ~singular_exterior
+        % we had a paired exterior
+        roi_data(roi_data(1,:)==0+delta,:)=[];
+    end
+    n_ROIs=size(roi_data,1);
+end
+clear singular_exterior;
+roi_data=sortrows(roi_data,1);
+%% guess delta if need be
+if ~exist('delta','var')
+    delta=roi_data(end,1)-roi_data(end/2,1);
+end
+%% connect the low/high roi vals via delta
+max_roi=max(roi_data(:,1));
+% correlated input will be  num lr rois  x  4  
+% element 1 is low  roi num 
+% element 2 is low  roi val
+% element 3 is high roi val
+% element 4 is high roi num 
+correlated_input=sparse(max_roi,4);
+for i_a_rois=1:n_ROIs
+    % roi - delta helps us know when a piece of data is found.
+    % Mostly for when delta is not n lr rois
+    roi_n1=roi_data(i_a_rois,1)-delta;
+    roi_L=roi_data(i_a_rois,1);
+    roi_H=roi_data(i_a_rois,1)+delta;
+    if correlated_input(roi_L,1)==0 ...
+        && ( roi_n1<1 || correlated_input(roi_n1,1)==0 )
+        correlated_input(roi_L,1)=roi_L;
+        correlated_input(roi_L,2)=roi_data(i_a_rois,2);
+        correlated_input(roi_L,3)=roi_data(roi_data(:,1)==roi_H,2);
+        correlated_input(roi_L,4)=roi_H;
+    end
+    %{
+    % superfluous print per roi, used in debug
+    fprintf('.');
+    if ~mod(i_a_rois,80)
+        fprintf('\n');
+    end
+    %}
+end
+%{
+% superfluous print per roi finalizer, used in debug
+if mod(i_a_rois,80)
+    fprintf('\n');
+end
+%}
+correlated_input=full(correlated_input(correlated_input(:,1)~=0,:));
+%% calculate our CoV from elements 2 and 3 of the correlated input.
+CoV_array=zeros([3,size(correlated_input,1)]);
+CoV_array(1,:)=correlated_input(:,1);
+% It is important that the second std option be set to 0 to match what Nian did in Excel.
+CoV_array(2,:)=std(correlated_input(:,2:3),0,2)./mean(correlated_input(:,2:3),2);
+CoV_array(3,:)=mean(correlated_input(:,2:3),2);
+end
 
