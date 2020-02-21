63e15ce5b9a2eb51a6edfc13c0d51cff11439b58
James Cook <jamesjcook@users.noreply.github.com>
2020-02-21 14:58:48 -0500

    Fixed some missing path issues in compiling

--- matlab_execs_for_SAMBA_src/compile_commands/compile__pathset.m
+++ matlab_execs_for_SAMBA_src/compile_commands/compile__pathset.m
@@ -3,3 +3,5 @@ addpath(fullfile(pdir,''));
 addpath(fullfile(pdir,'..'));
 addpath(fullfile(pdir,'..','fdr_bh'));
 addpath(fullfile(pdir,'..','20130227_xlwrite'));
+addpath('/cm/shared/workstation_code_dev/shared/mathworks/wildcardsearch');
+addpath(fullfile(getenv('WKS_SHARED'),'civm_matlab_common_utils'));
--- matlab_execs_for_SAMBA_src/compile_commands/compile_command_for_write_stats.m
+++ matlab_execs_for_SAMBA_src/compile_commands/compile_command_for_write_stats.m
@@ -1,7 +1,11 @@
 function compile_command_for_write_stats()
 
 gotcha_cache=fullfile(fileparts(which('write_individual_stats_exec')),sprintf('%s_gotchas.txt','write_individual_stats_exec'));
-include_files = {gotcha_cache};
+
+include_files = {gotcha_cache
+    '/cm/shared/workstation_code_dev/shared/mathworks/wildcardsearch/regexpdir.m'
+
+};
 compile_command__allpurpose('write_individual_stats_exec',include_files,'')
 
 return
