1473c49df16b76fbca29d3f6a5f9f60bb83c6727
James Cook <jamesjcook@users.noreply.github.com>
2020-11-11 13:51:38 -0500

    Patched missing nifti tools in compile pathsetter

--- matlab_execs_for_SAMBA_src/compile_commands/compile__pathset.m
+++ matlab_execs_for_SAMBA_src/compile_commands/compile__pathset.m
@@ -3,5 +3,6 @@ addpath(fullfile(pdir,''));
 addpath(fullfile(pdir,'..'));
 addpath(fullfile(pdir,'..','fdr_bh'));
 addpath(fullfile(pdir,'..','20130227_xlwrite'));
+addpath('/cm/shared/workstation_code_dev/shared/mathworks/NIFTI_20140122');
 addpath('/cm/shared/workstation_code_dev/shared/mathworks/wildcardsearch');
 addpath(fullfile(getenv('WKS_SHARED'),'civm_matlab_common_utils'));
