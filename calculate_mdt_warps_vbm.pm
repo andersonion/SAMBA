946149c547af7f78624f675c1d6d220f015983c4
James Cook <jamesjcook@users.noreply.github.com>
2021-06-03 16:24:00 -0400

    Updated to allow proceede even if diff when debugging is high

--- calculate_mdt_warps_vbm.pm
+++ calculate_mdt_warps_vbm.pm
@@ -462,7 +462,15 @@ sub calculate_mdt_warps_vbm_Runtime_check {
                         $template_match = 1;
                     } else {
                         print " $PM: ${Hf_comp}\n"; # Is this the right place for this?
-                        sleep_with_countdown(3);
+			if($debug_val< 100 ){
+			    sleep_with_countdown(3);
+			} else {
+			    my $ignore_diff='';
+			    do { 
+				$ignore_diff=user_prompt("Are these differences okay? (y/N)");
+			    } while($ignore_diff !~ m/^y(es)?|n(o)?$/ix);
+			    $template_match = 1 if $ignore_diff =~ m/^y(es)?$/ix;
+			}
                     }
                 } else {
                     $template_match = 1;
