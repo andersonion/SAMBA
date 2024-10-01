6359908fb804f34167a8fbc54f4d4bd021df34cf
BJ Anderson <floydmitchell@hotmail.com>
2024-10-01 13:36:29 -0400

    Install script working up to installing all perl dependencies.

--- SAMBA_setup.bash
+++ SAMBA_setup.bash
@@ -2,7 +2,11 @@
 
 # If you have admin privileges, you can set these variables cluster-wide.
 # In that case provide the full path to the script in /etc/profile.d/ as the first argument.
-
+DEBUG=0;
+db=' 2>/dev/null ';
+if (($DEBUG));then
+	db='';
+fi
 profiled_script=$1;
 update_bashrc=1;
 if [[ ${profiled_script:0:15} == "/etc/profile.d/" ]];then
@@ -29,7 +33,6 @@ fi
 SAD=${SAMBA_APPS_DIR};
 
 pb_test=$(which perlbrew  2> /dev/null | wc -l);
-echo "perlbrew test = ${pb_test}";
 
 src_file=${SAD}/perl5/etc/bashrc;
 
@@ -70,78 +73,75 @@ else
 	echo "Perlbrew installation has appeared to have failed." && exit 1
 fi
 pb_test=$(which perlbrew  2> /dev/null | wc -l);
-echo "perlbrew test = ${pb_test}";
-echo "if perlbrew test is greater than 0, then you have been successful!"
+if [[ ${pb_test} -gt 0 ]];then
+	echo "Perlbrew has been successfully installed."
+else
+	echo "Perlbrew has NOT been installed;";
+	echo "Rerunning in debug mode to aid in troubleshooting..."
+	export PERLBREW_ROOT=${SAD}/perl5;
+	curl -L http://install.perlbrew.pl | bash 1> /dev/null;
+fi	
 ######
 
 list_test=$(perlbrew list 2>/dev/null | wc -l);
-echo "list_test = x${list_test}x"
+
 if [[ ${list_test} != 0 ]];then
-	list_test=$(perlbrew list | grep -5.16.3 | wc -l);
-	echo "perl-5.16.3_test = ${list_test}";
+	eval perlbrew use perl-5.16.3 ${db};
+	list_test=$(perlbrew list | grep "\-5.16.3" | wc -l);
 fi
 
-if ((! ${list_test}));then
-	perlbrew --notest install  perl-5.16.3
+if ((${list_test}));then
+	echo "Perl-5.16.3 has been successfully installed."
+else
+	echo "Perl-5.16.3 is not installed.";
+	echo "Attempting to install it now..."
+	eval perlbrew --notest install  perl-5.16.3 ${db}
+	eval perlbrew use perl-5.16.3 ${db};
+	list_test=$(perlbrew list | grep "\-5.16.3" | wc -l);
+	if ((${list_test}));then
+		echo "Perl-5.16.3 has been successfully installed.";
+	else
+		echo "Perl-5.16.3 has NOT been installed;";
+		echo "Rerunning in debug mode to aid in troubleshooting..."
+		perlbrew --notest install  perl-5.16.3
+	fi	
 fi
-perlbrew lib create perl-5.16.3@SAMBA
+
+# Do we actually use perl-5.16.3@SAMBA??
+eval perlbrew lib create perl-5.16.3@SAMBA ${db}
+
+# Test to see which 'use' command we want.
+#perlbrew use perl-5.16.3@SAMBA
 perlbrew use perl-5.16.3
 
-perlbrew install-cpanm
-
+eval perlbrew install-cpanm ${db}
 
 carton_test=$(which carton  2> /dev/null | wc -l);
-
-echo "carton_test = x${carton_test}x"
-if [[ ${carton_test} != 0 ]];then
-	cpanm install Carton;
-fi
-carton_test=$(which carton  2> /dev/null | wc -l);
-echo "carton_test = x${carton_test}x"
-
-######
-if ((0));then
-carton_test=$(which carton  2> /dev/null | wc -l);
-
-src_file=${SAD}/perl5/etc/bashrc;
-
-if [[ ${carton_test} == 0 ]];then
-	cd ${SAMBA_APPS_DIR};
-	git clone https://github.com/perl-carton/carton.git
-	touch cpanfile
-	touch cpanfile.snapshot
-fi
-
-
-if [[ -f ${src_file} ]];then
-	src_file=$(ls ${src_file});
-	src_cmd="source ${src_file}";
-	  
-	
-	if ((${update_bashrc}));then
-		src_test=$(grep "${src_cmd}" ~/.bashrc | wc -l);
-		if [[ ${src_test} == 0 ]]; then
-			echo "Adding local version of perlbrew to ~/.bashrc";
-			echo "# Adding local version of perlbrew:" >> ~/.bashrc;
-			echo ${src_cmd} >> ~/.bashrc;
-		fi
-	else
-		src_test=$(grep "${src_cmd}" ${profiled_script} | wc -l);
-		if [[ ${src_test} == 0 ]]; then
-			echo "Adding local version of perlbrew to ${profiled_script}";
-			echo "# Adding local version of perlbrew:" | sudo tee -a ${profiled_script};
-			echo ${src_cmd} | sudo tee -a ${profiled_script};
-		fi   
-	fi
-
-${src_cmd};
+if [[ ${carton_test} -gt 0 ]];then
+	echo "Carton is found and ready to do work."
 else
-	echo "Cannot find ${src_file}";
-	echo "Perlbrew installation has appeared to have failed." && exit 1
+	echo "Carton not found. Attempting to install now..."
+	if [[ ! -f ${SAMBA_APPS_DIR}/carton ]];then
+		echo "Carton directory not found; attempting to clone from github;"
+		echo "Repository: https://github.com/perl-carton/carton.git"
+		cd ${SAMBA_APPS_DIR};
+		git clone https://github.com/perl-carton/carton.git
+		touch cpanfile
+		touch cpanfile.snapshot
+	fi
+	eval cpanm -f Carton ${db}
+	#cpanm install Carton;
+	
+	
+	carton_test=$(which carton  2> /dev/null | wc -l);
+	if [[ ${carton_test} -gt 0 ]];then
+		echo "Carton is found and ready to do work."
+	else
+		echo "Carton has not been successfully instaleld;"
+		echo "Rerunning in debug mode to aid in troubleshooting..."
+	fi
 fi
-pb_test=$(which perlbrew  2> /dev/null | wc -l);
-echo "perlbrew test = ${pb_test}";
-echo "if perlbrew test is greater than 0, then you have been successful!"
 
 
-fi
+cd ${SAMBA_APPS_DIR}/SAMBA
+carton install
