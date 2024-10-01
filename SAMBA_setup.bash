28b77e5c99457908b5dcc92ad2ea5e1e68186118
BJ Anderson <floydmitchell@hotmail.com>
2024-10-01 16:54:10 -0400

    SAMBA setup script mostly complete except a test run.

--- SAMBA_setup.bash
+++ SAMBA_setup.bash
@@ -13,24 +13,88 @@ if [[ ${profiled_script:0:15} == "/etc/profile.d/" ]];then
 	update_bashrc=0;
 fi
 
-
-if [[ "xx" == "x${SAMBA_APPS_DIR}x" ]];then
-	echo "Environment variable 'SAMBA_APPS_DIR' has not been defined."
-	echo "Either define this in your ~/.bashrc file OR"
-	echo "if you have sudo/admin privileges, in a script in /etc/profile.d/"
-	echo "The line should look something like:"
-	echo "'export SAMBA_APP_DIR=/SAMBAs/parent/folder/'"
-	echo "Quitting without doing work now..." && exit 1
-fi
-
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
+source ${SCRIPTPATH}/bashrc_for_SAMBA
 
 if [[ ! -d ${SAMBA_APPS_DIR} ]];then
 	echo "Environment variable 'SAMBA_APPS_DIR' is set, but does not exist."
+	echo "Please double-check that you have set it appropriately."
 	echo "SAMBA_APPS_DIR=${SAMBA_APPS_DIR}" && exit 1
 fi
 
 # Let's making coding easier...
 SAD=${SAMBA_APPS_DIR};
+
+
+function append_startup_script(){
+	local msg=$1;
+	local bashrc_cmd=$2;
+	local bashrc_msg=$3;
+	#local  __resultvar=$2;
+	#local c_number='';
+	
+	if ((${update_bashrc}));then
+		src_test=$(grep "${bashrc_cmd}" ~/.bashrc | wc -l);
+		if [[ ${src_test} == 0 ]]; then
+			echo ${msg} " ~/.bashrc";
+			if [[ ${bashrc_msg} ]];then
+				echo "# ${bashrc_msg}" >> ~/.bashrc;
+			fi
+			echo ${bashrc_cmd} >> ~/.bashrc;
+		fi
+	else
+		src_test=$(grep "${bashrc_cmd}" ${profiled_script} | wc -l);
+		if [[ ${src_test} == 0 ]]; then
+			echo ${msg} " ${profiled_script}";
+			if [[ ${bashrc_msg} ]];then
+				echo "# ${bashrc_msg}" | sudo tee -a ${profiled_script};
+			fi
+			echo ${bashrc_cmd} | sudo tee -a ${profiled_script};
+		fi   
+	fi
+  	#eval $__resultvar="'${c_number}'";
+}
+
+######
+# Install matlab_execs and local MATLAB
+mefs=${MATLAB_EXEC_PATH}
+matlab_test=$(ls ${mefs} 2> /dev/null | wc -l);
+if [[ ${matlab_test} == 0 ]];then
+	echo "matlab_execs not found. Attempting to install now..."
+	if [[ ! -f ${mefs} ]];then
+		echo "matlab_execs directory not found; attempting to clone from github;"
+		echo "Repository: https://github.com/andersonion/matlab_execs_for_SAMBA.git"
+		cd ${SAMBA_APPS_DIR};
+		git clone https://github.com/andersonion/matlab_execs_for_SAMBA.git
+	fi
+
+fi 
+
+matlab_test=$(ls ${mefs} 2> /dev/null | wc -l);
+if [[ ${matlab_test} == 0 ]];then
+	echo "matlab_execs folder has NOT been installed;"
+	echo "Please check your permissions, etc, and try again."
+else
+	echo "matlab_execs folder is installed."
+	M2P=${MATLAB_2015b_PATH}
+	m2015b_test=$(ls ${M2P} 2> /dev/null | wc -l);
+	if [[ ${m2015b_test} == 0 ]];then
+		echo "MATLAB2015b not found. Attempting to install now..."
+		bash ${mefs}/install_matlab_for_SAMBA
+	fi
+fi
+
+m2015b_test=$(ls ${M2P} 2> /dev/null | wc -l);
+if [[ ${m2015b_test} == 0 ]];then
+	echo "MATLAB2015b has NOT been installed;"
+	echo "Please check your permissions, etc, and try again."
+else
+	echo "MATLAB2015b is instslled."
+fi
+
+
+######
+## Install perlbrew
 
 pb_test=$(which perlbrew  2> /dev/null | wc -l);
 
@@ -49,23 +113,26 @@ fi
 if [[ -f ${src_file} ]];then
 	src_file=$(ls ${src_file});
 	src_cmd="source ${src_file}";
-	  
 	
-	if ((${update_bashrc}));then
-		src_test=$(grep "${src_cmd}" ~/.bashrc | wc -l);
-		if [[ ${src_test} == 0 ]]; then
-			echo "Adding local version of perlbrew to ~/.bashrc";
-			echo "# Adding local version of perlbrew:" >> ~/.bashrc;
-			echo ${src_cmd} >> ~/.bashrc;
-		fi
-	else
-		src_test=$(grep "${src_cmd}" ${profiled_script} | wc -l);
-		if [[ ${src_test} == 0 ]]; then
-			echo "Adding local version of perlbrew to ${profiled_script}";
-			echo "# Adding local version of perlbrew:" | sudo tee -a ${profiled_script};
-			echo ${src_cmd} | sudo tee -a ${profiled_script};
-		fi   
-	fi
+	msg="Adding local version of perlbrew to";
+	comment="Adding local version of perlbrew:"
+	
+	append_startup_script "${msg}" "${src_cmd}" "${comment}"
+#	if ((${update_bashrc}));then
+#		src_test=$(grep "${src_cmd}" ~/.bashrc | wc -l);
+#		if [[ ${src_test} == 0 ]]; then
+#			echo "Adding local version of perlbrew to ~/.bashrc";
+#			echo "# Adding local version of perlbrew:" >> ~/.bashrc;
+#			echo ${src_cmd} >> ~/.bashrc;
+#		fi
+#	else
+#		src_test=$(grep "${src_cmd}" ${profiled_script} | wc -l);
+#		if [[ ${src_test} == 0 ]]; then
+#			echo "Adding local version of perlbrew to ${profiled_script}";
+#			echo "# Adding local version of perlbrew:" | sudo tee -a ${profiled_script};
+#			echo ${src_cmd} | sudo tee -a ${profiled_script};
+#		fi   
+#	fi
 
 ${src_cmd};
 else
