fe532f116eeba01fe3fc80f2a80497f3e3cd5b89
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-02-14 14:22:35 -0500

    Debugging cluster_test bash script.

--- cluster_test.bash
+++ cluster_test.bash
@@ -8,7 +8,7 @@ cluster=0;
 SGE_cluster=$(qstat  2>&1 | grep 'command not found' | wc -l | tr -d [:space:]);
 slurm_cluster=$(sbatch --help  2>&1 | grep 'command not found' | wc -l | tr -d [:space:]);
 # This returns '1' if NOT on a cluster, so let's reverse that...
-if ((! ${slurm_cluster}));then && 
+if ((! ${slurm_cluster}));then
 	cluster=1;
 elif ((! ${SGE_cluster}))
 	cluster=2;
