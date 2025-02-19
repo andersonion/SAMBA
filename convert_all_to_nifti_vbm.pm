249a5bdbe4a561eb2b76d629ad0dcdbf304747ef
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-02-19 15:48:52 -0500

    Debugging handling of slurm\'s scontrol arguments.

--- convert_all_to_nifti_vbm.pm
+++ convert_all_to_nifti_vbm.pm
@@ -363,18 +363,18 @@ sub convert_all_to_nifti_vbm_Runtime_check {
     $ch_runlist = $Hf->get_value('channel_comma_list');
     @channel_array = split(',',$ch_runlist);
 
-    #my $case = 1;
-    #my ($dummy,$skip_message)=convert_all_to_nifti_Output_check($case);
-
-    #if ($skip_message ne '') {
-    #    print "${skip_message}";
-    #}
-
-	my $case = 2;
+    my $case = 1;
     my ($dummy,$skip_message)=convert_all_to_nifti_Output_check($case);
+
     if ($skip_message ne '') {
         print "${skip_message}";
     }
+
+	#my $case = 2;
+    #my ($dummy,$skip_message)=convert_all_to_nifti_Output_check($case);
+    #if ($skip_message ne '') {
+    #    print "${skip_message}";
+    #}
 }
 
 
