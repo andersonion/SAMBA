5eb086e8467f12b79852a0a9976451b36b871ab9
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-02-19 16:06:11 -0500

    Debugging handling of slurm\'s scontrol arguments.

--- convert_all_to_nifti_vbm.pm
+++ convert_all_to_nifti_vbm.pm
@@ -125,8 +125,9 @@ print STDOUT  " Checkpoint 4. x${error_message}x\n";
 	if (($error_message eq '')) {
 		error_out("${error_message}",0);
 	}
-
+print STDOUT  " Checkpoint 5. x${error_message}x\n";
     my $real_time = vbm_write_stats_for_pm($PM,$Hf,$start_time);
+    print STDOUT  " Checkpoint 6. x${error_message}x\n";
     print "$PM took ${real_time} seconds to complete.\n";
     if ($error_message ne '') {
         error_out("${error_message}",0);
