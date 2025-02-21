b858f7927b5fbc338faf7a4f89074902e47bb6d2
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-02-21 16:36:05 -0500

    Debugging get_nii_from_inputs.

--- SAMBA_pipeline_utilities.pm
+++ SAMBA_pipeline_utilities.pm
@@ -1402,7 +1402,7 @@ if (0) {
         	print(join("\n",@input_files_0),"\n");
         }
         if ((! defined $input_file) || ($input_file eq '') ) {
-            my @input_files_1= grep(/^($runno).*(${test_contrast})\.($valid_formats_string){1}(\.gz)?$/i ,readdir(DIR)); #27 Dec 2016, added "^" because new phantom naming method of prepending (NOT substituting) "P" "Q" etc to beginning of runno results in ambiguous selection of files. Runno "S64944" might return "PS64944" "QS64944" or "S64944".
+            my @input_files_1= grep(/^${runno}.*${test_contrast}\.($valid_formats_string)(\.gz)?$/i ,readdir(DIR)); #27 Dec 2016, added "^" because new phantom naming method of prepending (NOT substituting) "P" "Q" etc to beginning of runno results in ambiguous selection of files. Runno "S64944" might return "PS64944" "QS64944" or "S64944".
             
             $input_file = $input_files_1[0];
                     if ( defined $input_file) { 
