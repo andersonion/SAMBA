18ff279c53eea8a676d361a2c40017f95a1527ce
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-04-02 15:01:56 -0400

    Debugging label lookup table handling.

--- warp_atlas_labels_vbm.pm
+++ warp_atlas_labels_vbm.pm
@@ -782,19 +782,17 @@ sub warp_atlas_labels_vbm_Runtime_check {
         # 2 April 2025 Going to force it to look for only .txt files.
 
         my $local_lookup = $Hf->get_value("${runno}_${label_atlas_nickname}_label_lookup_table");
-        print "1111: ${local_lookup}\n";
         if ($local_lookup eq 'NO_KEY') {
             #my $local_pattern="^${runno}_${label_atlas_nickname}_${label_type}_lookup[.].*\$";
             my $local_pattern="^${runno}_${label_atlas_nickname}_${label_type}_lookup[.].txt\$";
             ($local_lookup) = find_file_by_pattern($current_path,$local_pattern);
-            print "2222: ${local_lookup}\n";
             if ((defined $local_lookup) && ( -e $local_lookup) ) {
                 $Hf->set_value("${runno}_${label_atlas_nickname}_label_lookup_table",$local_lookup);
                 print "3333: ${local_lookup}\n";
             } else {
                 my ($atlas_label_dir, $dummy_1, $dummy_2) = fileparts($atlas_label_path,2);
                 if ( -d $atlas_label_dir) {
-                    my $pattern = "^.*lookup[.].*\$";
+                    my $pattern = "^.*lookup[.].txt\$";
                     my ($source_lookup) = find_file_by_pattern($atlas_label_dir,$pattern);
                     if ((defined $source_lookup) && ( -e $source_lookup)) {
                         my ($aa,$bb,$ext)=fileparts($source_lookup,2);
@@ -802,7 +800,8 @@ sub warp_atlas_labels_vbm_Runtime_check {
                     }
                     ($local_lookup) = find_file_by_pattern($current_path,$local_pattern);
                     if ((defined $local_lookup) && ( -e $local_lookup) ) {
-                        $Hf->set_value("${runno}_${label_atlas_nickname}_label_lookup_table",$local_lookup);
+						print "4444: ${local_lookup}\n";
+                        $Hf->set_value("${runno}_${label_atlas_nickname}_${label_type}_lookup_table",$local_lookup);
                     }
                 }
             }
