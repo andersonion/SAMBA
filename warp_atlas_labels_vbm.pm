d46f1dbd244fd8c763207b74fd3bedbc4b6b3319
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-04-02 15:12:19 -0400

    Debugging label lookup table handling.

--- warp_atlas_labels_vbm.pm
+++ warp_atlas_labels_vbm.pm
@@ -782,10 +782,12 @@ sub warp_atlas_labels_vbm_Runtime_check {
         # 2 April 2025 Going to force it to look for only .txt files.
 
         my $local_lookup = $Hf->get_value("${runno}_${label_atlas_nickname}_label_lookup_table");
+        print "1111: ${local_lookup}\n";
         if ($local_lookup eq 'NO_KEY') {
             #my $local_pattern="^${runno}_${label_atlas_nickname}_${label_type}_lookup[.].*\$";
             my $local_pattern="^${runno}_${label_atlas_nickname}_${label_type}_lookup[.].txt\$";
             ($local_lookup) = find_file_by_pattern($current_path,$local_pattern);
+            print "2222: ${local_lookup}\n";
             if ((defined $local_lookup) && ( -e $local_lookup) ) {
                 $Hf->set_value("${runno}_${label_atlas_nickname}_label_lookup_table",$local_lookup);
             } else {
@@ -801,7 +803,6 @@ sub warp_atlas_labels_vbm_Runtime_check {
                         `cp ${source_lookup} ${current_path}/${runno}_${label_atlas_nickname}_${label_type}_lookup${ext}`;
                     }
                     ($local_lookup) = find_file_by_pattern($current_path,$local_pattern);
-                    print "3333: ${local_lookup}\n";
                     if ((defined $local_lookup) && ( -e $local_lookup) ) {
 						print "4444: ${local_lookup}\n";
                         $Hf->set_value("${runno}_${label_atlas_nickname}_${label_type}_lookup_table",$local_lookup);
