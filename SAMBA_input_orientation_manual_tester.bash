b9c7e77636dc874bcae5e8fbb8372a558e7b25ce
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-04-02 12:22:36 -0400

    Debugging orientation tester.

--- SAMBA_input_orientation_manual_tester.bash
+++ SAMBA_input_orientation_manual_tester.bash
@@ -114,7 +114,7 @@ if [[ ${cluster_code} -gt 0 ]];then
 	cmd_2="ovs_2=\$(PrintHeader ${target} 1 | cut -d  'x' -f2);b=\$( bc -l <<<\"8*\$ovs_2 \" );";
 	cmd_3="ovs_3=\$(PrintHeader ${target} 1 | cut -d  'x' -f3);c=\$( bc -l <<<\"8*\$ovs_3 \" );";
 	cmd_4="if [[ ! -f ${input} ]];then ResampleImageBySpacing 3 ${image} ${input} \${a} \${b} \${c} 0; fi;";
-	cmd_5="if [[ ! -f ${ds_target} ]];then ResampleImageBySpacing 3 ${target_folder}/${target_name} ${ds_target} ${a} ${b} ${c} 0;fi;";
+	cmd_5="if [[ ! -f ${ds_target} ]];then ResampleImageBySpacing 3 ${target_folder}/${target_name} ${ds_target} \${a} \${b} \${c} 0;fi;";
 	
 	cmd="${cmd_1}${cmd_2}${cmd_3}${cmd_4}${cmd_5}";
 	name="${i_name}_prep_work";
